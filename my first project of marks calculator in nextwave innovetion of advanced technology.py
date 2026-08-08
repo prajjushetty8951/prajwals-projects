3be6ada33f2ec750857204090c2909797b07a4e0
prajwal shetty j <prajjushetty8951@gmail.com>
2026-08-08 22:30:28 +0530

    Add files via upload

---
+++ my first project of marks calculator in nextwave innovetion of advanced technology.py
@@ -0,0 +1,24 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+============== RESTART: C:/Users/PRAJWAL SHETTY J/prajjushetty.py ==============
+       STUDENT GRADE CALCULATOR
+Enter student name: prajwal
+Enter English marks: 45
+Enter Maths marks: 45
+Enter Gen AI marks: 85
+Enter Aptitude marks: 85
+Enter Back End marks: 85
+Enter Front End marks: 85
+             STUDENT RESULT
+Student Name : prajwal
+English      : 45.0
+Maths        : 45.0
+Gen AI       : 85.0
+Aptitude     : 85.0
+Back End     : 85.0
+Front End    : 85.0
+Total Marks  : 430.0 / 600
+Percentage   : 71.66666666666667 %
+Grade        : B
+Result       : PASS
